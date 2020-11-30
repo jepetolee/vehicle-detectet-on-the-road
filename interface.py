e3c1663a40af59aefebb88f00d2476316a448f88
jepetolee <55366212+jepetolee@users.noreply.github.com>
2020-11-30 19:41:22 +0900

    update

--- interface.py
+++ interface.py
@@ -1,13 +1,69 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
-
+from PyQt5.QtWidgets import *
 import argparse
 
 import torch.backends.cudnn as cudnn
-
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import smtplib
 from models.experimental import *
 from utils.datasets import *
 from utils.utils import *
+
+# SMTP 접속을 위한 서버, 계정 설정
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 465
+# 보내는 메일 계정
+SMTP_USER = "개인 이메일로 기제"
+SMTP_PASSWORD = "개인 아이디로 기제"
+
+
+# 이메일 유효성 검사 함수
+def is_valid(addr):
+    import re
+    if re.match('(^[a-zA-Z-0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)', addr):
+        return True
+    else:
+        return False
+
+
+# 이메일 보내기 함수
+def send_mail(addr, subj_layout, cont_layout, attachment=None):
+    if not is_valid(addr):
+        print("Wrong email: " + addr)
+        return
+
+    # 텍스트 파일
+    msg = MIMEMultipart("alternative")
+    # 첨부파일이 있는 경우 mixed로 multipart 생성
+    if attachment:
+        msg = MIMEMultipart('mixed')
+    msg["From"] = SMTP_USER
+    msg["To"] = addr
+    msg["Subject"] = subj_layout
+    contents = cont_layout
+    text = MIMEText(_text=contents, _charset="utf-8")
+    msg.attach(text)
+    # 첨부파일이 있으면
+    if attachment:
+        from email.mime.base import MIMEBase
+        from email import encoders
+        file_data = MIMEBase("application", "octect-stream")
+        file_data.set_payload(open(attachment, "rb").read())
+        encoders.encode_base64(file_data)
+        import os
+        filename = os.path.basename(attachment)
+        file_data.add_header("Content-Disposition", 'attachment', filename=('UTF-8', '', filename))
+        msg.attach(file_data)
+    # smtp로 접속할 서버 정보를 가진 클래스변수 생성
+    smtp = smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT)
+    # 해당 서버로 로그인
+    smtp.login(SMTP_USER, SMTP_PASSWORD)
+    # 메일 발송
+    smtp.sendmail(SMTP_USER, addr, msg.as_string())
+    # 닫기
+    smtp.close()
 
 
 def detect(save_img=False):
@@ -137,7 +193,6 @@ def detect(save_img=False):
     print('Done. (%.3fs)' % (time.time() - t0))
 
 
-
 class MyApp(QWidget):
 
     def __init__(self):
@@ -145,18 +200,38 @@ class MyApp(QWidget):
         self.initUI()
 
     def initUI(self):
+
         self.setWindowTitle('대포차 검진기')
         self.move(300, 300)
         self.resize(1280, 800)
+        label = QLabel("이메일을 적어주세요", self)
+        label.move(640, 250)
+        self.email = QLineEdit("", self)
+        self.email.move(540, 300)
+        self.email.resize(300, 20)
         btn2 = QPushButton(self)
-        btn2.setText('Button&2')
-        vbox = QVBoxLayout()
-        vbox.addWidget(btn2)
+        btn2.clicked.connect(self.start)
+        btn2.setText('사진 이메일 보내기')
+        self.vbox = QWidget
+        btn2.move(590, 330)
+        btn2.resize(200, 100)
         self.show()
+
+    def start(self):
+        if self.email.text() == "":
+            QMessageBox.warning(self.vbox, "Warning", "Are you sure you leave?")
+        else:
+            with torch.no_grad():
+                if opt.update:  # update all models (to fix SourceChangeWarning)
+                    for opt.weights in ['yolov5s.pt', 'yolov5m.pt', 'yolov5l.pt', 'yolov5x.pt', 'yolov3-spp.pt']:
+                        detect()
+                        create_pretrained(opt.weights, opt.weights)
+                else:
+                    detect()
+            send_mail(self.email.text(),'Car_detected','Here is result','D:/vehicle-detection-on-the-road/inference/output/images.jpg')
 
 
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', nargs='+', type=str, default='best.pt', help='model.pt path(s)')
     parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
@@ -172,15 +247,7 @@ if __name__ == '__main__':
     parser.add_argument('--augment', action='store_true', help='augmented inference')
     parser.add_argument('--update', action='store_true', help='update all models')
     opt = parser.parse_args()
-    print(opt)
 
-    with torch.no_grad():
-        if opt.update:  # update all models (to fix SourceChangeWarning)
-            for opt.weights in ['yolov5s.pt', 'yolov5m.pt', 'yolov5l.pt', 'yolov5x.pt', 'yolov3-spp.pt']:
-                detect()
-                create_pretrained(opt.weights, opt.weights)
-        else:
-            detect()
     app = QApplication(sys.argv)
     ex = MyApp()
     sys.exit(app.exec_())
